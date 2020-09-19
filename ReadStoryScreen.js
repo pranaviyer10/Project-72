41c87607ca4a50f4cd9d0a2c071b2a0028f07f28
pranaviyer10 <66201939+pranaviyer10@users.noreply.github.com>
2020-09-19 10:53:00 +0530

    Add files via upload

---
+++ ReadStoryScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import db from '../config';
+
+export default class ReadScreen extends React.Component {
+    render() {
+        return(
+            <View>
+                <Text>Search</Text>
+            </View>
+        )
+    }
+}
